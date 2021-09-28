7b8455ea5f03a7e592e5581db89dfc243a81c98d
minji.hong <minji.hong@bespinglobal.com>
2021-09-28 16:49:21 +0900

    edit appspec.yml

---
+++ scripts/applicationStart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "applicationStart"
+
+cd /home/ubuntu/app
+
+java -jar app-0.0.1-SNAPSHOT.war
--- src/main/resources/scripts/applicationStart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "applicationStart"
-
-cd '/home/ec2-user/app'
-nohup java -jar app-0.0.1-SNAPSHOT.war
